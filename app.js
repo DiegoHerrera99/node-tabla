9a4b7be95cecd6bc45d8c98cc65e51a6c36b678a
Diego Herrera <dherreramorales99@gmail.com>
2022-11-07 13:54:16 -0600

    Readme Agregado

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ const colors = require('colors')
 console.clear()
 
 crearArchivoTabla(argv.b, argv.h, argv.l)
-    .then(nombreArchivo => console.log(nombreArchivo.bold.bgGreen, 'creado!'.bold.bgGreen))
+    .then(nombreArchivo => console.log(`Archivo ${nombreArchivo.bgGreen} creado con exito!`.bold))
     .catch(err => console.log(err))
 
